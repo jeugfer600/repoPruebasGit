14fd75606a59a4490d93c3d43d42d480ec26c53b
Jose Seda <jseda442@gmail.com>
2024-11-08 13:41:43 +0100

    metodo restar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    public static int restar(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
