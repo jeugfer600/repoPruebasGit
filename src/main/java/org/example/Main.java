58604636bf937feb3bda150073ea217fd1785feb
elpapass <115544415+elpapass@users.noreply.github.com>
2024-11-08 13:55:47 +0100

    Método Dividir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    public static float dividir(float a, float b) {
+        return a / b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
